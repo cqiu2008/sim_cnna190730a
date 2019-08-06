8bec5ced03d7f40f3b9a50a1137937ae2e81e0b6
cqiu <cqiu2008@hotmail.com>
2019-08-07 06:30:45 +0800

    ddd

--- rtl/sim/verilog/addsumram.v
+++ rtl/sim/verilog/addsumram.v
@@ -41,20 +41,40 @@ input                               I_clk           ,
 // ctrl bus
 input                               I_first_flag    ,
 input       [C_LENSIZE-1         :0]I_len           ,
-input                               I_dv_p2         ,
+input                               I_wr_pre3       ,
 // mem bus
-input       [C_DSIZE-1           :0]I_din           ,
-input                               I_dv            ,
+input       [C_DSIZE-1           :0]I_wdata         ,
+input                               I_wr            ,
+input                               I_wr_en         ,
 input       [C_ASIZE-1           :0]I_raddr         ,
-input                               I_rd            ,
 output reg  [C_DSIZE-1           :0]O_rdata             
 );
 
-//reg  [C_DSIZE-1           :0]I_wdata         ,
-//reg                          I_wr            ,
-//reg  [C_ASIZE-1           :0]I_raddr         ,
-//reg                          I_rd            ,
-//reg  [C_DSIZE-1           :0]O_rdata             
+reg  [C_ASIZE-1           :0]S_waddr    ;
+reg  [C_DSIZE-1           :0]S_wdata    ;       
+reg                          S_wr       ;        
+reg  [C_ASIZE-1           :0]S_raddr    ;        
+reg  [C_ASIZE-1           :0]S_rcnt     ;        
+reg                          S_rd       ;        
+wire [C_DSIZE-1           :0]S_rdata    ;           
+
+always @(posedge I_clk)begin
+    S_raddr <= I_wr_en ? S_rcnt : I_raddr   ; 
+end
+
+always @(posedge I_clk)begin
+    if(I_wr_en)begin
+        if(I_wr_pre3)begin
+            S_rcnt  <=  S_rcnt + {(C_ASIZE-1){1'b0},1'b1}   ;
+        end
+        else begin
+            S_rcnt  <=  S_rcnt                              ; 
+        end
+    end
+    else begin
+        S_rcnt  <=  {C_ASIZE{1'b0}}                         ;
+    end
+end
 
 sdpram #(
     .C_MEM_STYLE (C_MEM_STYLE   ),
@@ -62,14 +82,14 @@ sdpram #(
     .C_ASIZE     (C_ASIZE       ))
 u_sdpram(
     .I_wclk      (I_clk         ),
-    .I_waddr     (I_waddr       ),
+    .I_waddr     (S_waddr       ),
     .I_wdata     (I_wdata       ),
     .I_ce        (1'b1          ),
     .I_wr        (I_wr          ),
     .I_rclk      (I_clk         ),
-    .I_raddr     (I_raddr       ),
-    .I_rd        (I_rd          ),
-    .O_rdata     (O_rdata       )    
+    .I_raddr     (S_raddr       ),
+    .I_rd        (1'b1          ),
+    .O_rdata     (S_rdata       )    
 );
 
 
